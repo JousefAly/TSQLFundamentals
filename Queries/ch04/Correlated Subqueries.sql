e7a6e011ada2e228879e53d646877c63859eb637
JousefAly <youssefali00286@gmail.com>
2022-03-22 09:23:00 +0200

    Exsits predicate

--- Queries/ch04/Correlated Subqueries.sql
+++ Queries/ch04/Correlated Subqueries.sql
@@ -6,3 +6,12 @@ WHERE orderid =
 	(SELECT MAX(orderid)
 	 FROM Sales.Orders AS O2
 	 WHERE O2.custid = O1.custid);
+
+--return customers from spain who placed orders
+
+SELECT custid, companyname
+FROM Sales.Customers AS C
+WHERE country = N'Spain'
+	AND EXISTS 
+		(SELECT * FROM Sales.Orders AS O
+		 WHERE O.custid = C.custid);
